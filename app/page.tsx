292866bd6e58a5b7eac2e77b9d4231e9e92a23dd
Tim <timcoker100@gmail.com>
2026-05-10 10:08:18 +0100

    Simplify routing to fix 403 error

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,6 @@
 'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import LoginPage from './login/page';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const user = localStorage.getItem('currentUser');
-    
-    if (user) {
-      router.replace('/ladder');
-    } else {
-      router.replace('/login');
-    }
-  }, [router]);
-
-  return <div className="min-h-screen flex items-center justify-center">Redirecting to login...</div>;
+  return <LoginPage />;
 }

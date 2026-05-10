e1bbe503f15a3d23379e5dbdcc6cbe60ae4d69c9
Tim <timcoker100@gmail.com>
2026-05-10 10:52:56 +0100

    Final routing fix - direct to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const user = localStorage.getItem('currentUser');
-    if (user) {
-      router.replace('/ladder');
-    } else {
-      router.replace('/login');
-    }
-  }, [router]);
-
-  return <div className="min-h-screen bg-gray-50 flex items-center justify-center">Loading Napton Tennis Ladder...</div>;
+  redirect('/login');
 }

50e646dd373a9445a88c6633921076cc39925475
Tim <timcoker100@gmail.com>
2026-05-11 10:59:04 +0100

    Cleaned up database attempts - back to working version

--- db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-
-const client = createClient({ 
-  url: "file:./sqlite.db" 
-});
-
-export const db = drizzle(client);

a67247c61911b850e29aaf3038d16a8044469a74
Tim <timcoker100@gmail.com>
2026-05-20 15:04:47 +0100

    update supabase client config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://bdxbkpdssffogbltfyu.supabase.co'
 const supabaseAnonKey = 'sb_publishable_2J2AfzcpO_X7rHgkV7SXfg_jC3SNg-b'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+  },
+})

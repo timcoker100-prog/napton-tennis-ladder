fe9a4d3aefb1c7158ac0904f57ebca933f8a73f7
Tim <timcoker100@gmail.com>
2026-05-20 15:07:42 +0100

    improve supabase client

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,6 +5,8 @@ const supabaseAnonKey = 'sb_publishable_2J2AfzcpO_X7rHgkV7SXfg_jC3SNg-b'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
+    autoRefreshToken: true,
     persistSession: true,
-  },
+    detectSessionInUrl: false
+  }
 })

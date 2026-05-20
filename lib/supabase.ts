a53564de6db1b8f72776a9d1ffe797eefa17c823
Tim <timcoker100@gmail.com>
2026-05-20 15:50:13 +0100

    new supabase project final

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kwydtmxyqwzyiwbssdtq.supabase.co'
-const supabaseAnonKey = 'sb_publishable_tku2x3YWSnicqgko2mTlRw_p6DeOW62'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://kwydtmxyqwzyiwbssdtq.supabase.co',
+  'sb_publishable_tku2x3YWSnicqgko2mTlRw_p6DeOW62'
+)
